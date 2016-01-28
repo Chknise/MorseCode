da89165bf0c36457e6c4b30c52585138b899ffd4
ChuckH <chuckH@Zipcoder10s-MacBook-Pro.local>
2016-01-28 13:48:29 -0500

    first commit

---
+++ js/morseCode.js
@@ -0,0 +1,67 @@
+
+var chars = prompt("Please enter a word or phrase.");
+
+var charCodes=new Array(45); charCodes["a"]=". _";
+charCodes["b"]="_ . . .";
+charCodes["c"]="_ . _ .";
+charCodes["d"]="_ . .";
+charCodes["e"]=".";
+charCodes["f"]=". . _ .";
+charCodes["g"]="_ _ .";
+charCodes["h"]=". . . .";
+charCodes["i"]=". .";
+charCodes["j"]=". _ _ _";
+charCodes["k"]="_ . _";
+charCodes["l"]=". _ . .";
+charCodes["m"]="_ _";
+charCodes["n"]="_ .";
+charCodes["o"]="_ _ _";
+charCodes["p"]=". _ _ .";
+charCodes["q"]="_ _ . _";
+charCodes["r"]=". _ .";
+charCodes["s"]=". . .";
+charCodes["t"]="_";
+charCodes["u"]=". . _";
+charCodes["v"]=". . . _";
+charCodes["w"]=". _ _";
+charCodes["x"]="_ . . _";
+charCodes["y"]="_ . _ _";
+charCodes["z"]="_ _ . .";
+charCodes["1"]=". _ _ _ _";
+charCodes["2"]=". . _ _ _";
+charCodes["3"]=". . . _ _";
+charCodes["4"]=". . . . _";
+charCodes["5"]=". . . . .";
+charCodes["6"]="_ . . . .";
+charCodes["7"]="_ _ . . .";
+charCodes["8"]="_ _ _ . .";
+charCodes["9"]="_ _ _ _ .";
+charCodes["0"]="_ _ _ _ _";
+charCodes[","]="--..--";
+charCodes["."]=".-.-.-";
+charCodes["!"]="---.";
+charCodes["?"]="..--..";
+charCodes[";"]="-.-.-.";
+charCodes[":"]="---...";
+charCodes["'"]=".----.";
+charCodes["-"]="-....-";
+charCodes["("]="-.--.";
+charCodes[")"]="-.--.-";
+charCodes["["]="-.--.";
+charCodes["]"]="-.--.-";
+charCodes["/"]="-..-.";
+charCodes['"']=".-..-.";
+var temp='';
+
+
+for (a=0; a<chars.length; a++) {
+    if (chars[a]!=" ") {
+        temp+=charCodes[chars[a]]+" ";
+    }
+    else {
+    temp+=chars[a] + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp";
+    }
+}
+
+console.log(temp);
+document.getElementById('result').innerHTML = temp;
